0e044149d5095a42ca005a7beb78073902fd8d21
Thalita Gonçalves <thagsousa15@gmail.com>
2020-05-03 19:41:28 -0300

    tentativa de ordenação

--- src/data.js
+++ src/data.js
@@ -1,7 +1,20 @@
-import data from './data/rickandmorty/rickandmorty.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
-const sortBy = {
- // cipher: function 
-} 
+export const sortByAZ = (data) => {data.sort((a,z) => {
+    if (a.name > z.name) {
+        return 1;
+    }else{
+        return -1;
+    }
+})
+}
 
-export default
+/* export const sortByZA = () => {data.results.sort((a,z) => {
+    if (a.name < z.name) {
+        return 1;
+    } if (a.name > z.name) {
+        return -1;
+    }
+    return 0
+})
+} */
