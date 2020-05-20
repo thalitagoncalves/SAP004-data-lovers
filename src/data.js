7319b4bccb601dfcd5756bd5d61193c4da5c0a12
alessandramarinho <allemmarinho@gmail.com>
2020-05-20 11:07:31 -0300

    alterações no HTML, responsividade e README

--- src/data.js
+++ src/data.js
@@ -7,11 +7,9 @@ export const sortByZA = (data) => sortByAZ(data).reverse();
 export const filterData = (data,typeData, condition) => data.filter((character) => {
     return (character[typeData] === condition);
 })
-
 export const filterName = (data, search) => data.filter(character => {
     return character.name.toLowerCase().includes(search.toLowerCase());
 })
-
 export const computeStats = (data,typeData, condition) => data.reduce((initialType, totalType) => {
     return initialType + (totalType[typeData] === condition);
 },0)
